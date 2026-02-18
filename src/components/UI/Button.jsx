57bd7b2e83fdc11c7ed2ab9d62e29d3864c8b3b2
ilchyshynNazar <150625204+ilchyshynNazar@users.noreply.github.com>
2026-02-18 23:45:00 +0200

    Update Button.jsx

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -3,7 +3,7 @@ export default function Button({ children, active, onClick }) {
     <button
       className={`flex flex-col items-center p-3 rounded-xl transition-all
                   ${active ? "bg-purple-600 text-white" : "bg-gray-700 text-gray-300"}
-                  hover:bg-purple-500`}
+                  hover:bg-purple-550`}
       onClick={onClick}
     >
       {children}
