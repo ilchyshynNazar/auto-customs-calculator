336dd1a5ac6b0261a53a0ceafb481e33bd2bd2fb
ilchyshynNazar <150625204+ilchyshynNazar@users.noreply.github.com>
2026-02-25 11:01:58 +0200

    feat(ui): implement lifting hover effect with shadow and scale (#1)
    * feat(ui): implement lifting hover effect with shadow and scale
    
    * style: change main theme color to green

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -2,8 +2,8 @@ export default function Button({ children, active, onClick }) {
   return (
     <button
       className={`flex flex-col items-center p-3 rounded-xl transition-all
-                  ${active ? "bg-purple-600 text-white" : "bg-gray-700 text-gray-300"}
-                  hover:bg-purple-550`}
+                  ${active ? "bg-green-600 text-white" : "bg-gray-800 text-gray-400"}
+                  hover:bg-green-500`}
       onClick={onClick}
     >
       {children}
