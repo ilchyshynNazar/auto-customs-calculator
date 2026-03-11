64bd0fc0acc7e2a0227b36e7fd692822c98e375c
ilchyshynNazar <ilchyshynnazar0@gmail.com>
2026-03-11 20:02:08 +0200

    add posthog analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/App.css";
 import "./styles/Gradient.css";
+import posthog from 'posthog-js';
 
 console.log("main.jsx loaded");
 
@@ -14,3 +15,8 @@ ReactDOM.createRoot(root).render(
     <App />
   </React.StrictMode>
 );
+
+posthog.init('phc_5VCESRqRh3NNFwWvPNxXY1eKCvlPkHDzrKjzsEAe2Pk', {
+    api_host: 'https://eu.i.posthog.com',
+    defaults: '2026-01-30'
+})
